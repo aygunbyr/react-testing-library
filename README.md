1c07dd92019624a562d4878e672312adf5e608f3
Aygün Bayır <turkoaygun@gmail.com>
2023-09-09 15:01:49 +0300

    test: findBy queries

--- README.md
+++ README.md
@@ -36,7 +36,7 @@
 7. getByTitle
 8. getByTestId
 
-### queryBy and queryAllBy
+## queryBy and queryAllBy
 
 ### queryBy
 
@@ -47,3 +47,15 @@
 ### queryAllBy
 
 - Returns an array of all matching nodes for a query, and return an empty array if no elements match
+
+## findBy and findAllBy
+
+### findBy
+
+- Returns a Promise which resolves when an element is found which matches the given query
+- The promise is rejected if no element is found or if more than one element is found after a default timeout of 1000ms
+
+### findAllBy
+
+- Returns a promise which resolves to an array of elements when any elements are found which match the given query
+- The promise is rejected if no elements are found after a default timeout of 1000ms
--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Skills } from './skills';
 
 describe('Skills', () => {
@@ -30,5 +30,27 @@ describe('Skills', () => {
       name: 'Start Learning',
     });
     expect(startLearningButton).not.toBeInTheDocument();
+  });
+
+  test('Start learning button is eventually displayed', async () => {
+    render(<Skills skills={skills} />);
+
+    const loginButton = screen.getByRole('button', {
+      name: 'Login',
+    });
+
+    fireEvent.click(loginButton);
+
+    const startLearningButton = await screen.findByRole(
+      'button',
+      {
+        name: 'Start learning',
+      },
+      {
+        timeout: 2000,
+      }
+    );
+
+    expect(startLearningButton).toBeInTheDocument();
   });
 });
